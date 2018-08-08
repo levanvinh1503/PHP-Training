c32ca54be6545fea3e9c511711328c39ecf07960
levanvinh1503 <levanvinh1503@gmai.com>
2018-08-08 23:28:18 +0700

    fix email 08/08/2018 - 23:30

--- Week3/Mail/functionSendEmail.php
+++ Week3/Mail/functionSendEmail.php
@@ -1,16 +1,16 @@
 <?php
 if ($_POST['sendTo'] && $_POST['subjectEmail'] && $_POST['contentEmail']) {
-	$sendTo = $_POST['sendTo'];
-	$subjectEmail = $_POST['subjectEmail'];
-	$contentEmail = $_POST['contentEmail'];
-	$headerEmail = "MIME-Version: 1.0\r\n";
-	$headerEmail .= "Content-type: text/html\r\n";
-	$resultSendEmail = mb_send_mail($sendTo, $subjectEmail, $contentEmail, $headerEmail);
-	if( $resultSendEmail == true ) {
-		echo "Send email success !";
-	}else {
-		echo "Send email fail !";
-	}
+    $sendTo = $_POST['sendTo'];
+    $subjectEmail = $_POST['subjectEmail'];
+    $contentEmail = $_POST['contentEmail'];
+    $headerEmail = "MIME-Version: 1.0\r\n";
+    $headerEmail .= "Content-type: text/html\r\n";
+    $resultSendEmail = mb_send_mail($sendTo, $subjectEmail, $contentEmail, $headerEmail);
+    if ($resultSendEmail == true) {
+        echo "Send email success !";
+    } else {
+        echo "Send email fail !";
+    }
 } else {
-	echo "Input value !";
+    echo "Input value !";
 }
--- Week3/Mail/sendMail.php
+++ Week3/Mail/sendMail.php
@@ -1,41 +1,41 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Demo send email with mb_send_mail</title>
-	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <title>Demo send email with mb_send_mail</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 </head>
 <body>
-	<div class="container">
-		<h2>Send Email</h2>
-		<form method="POST" id="formEmail">
-			<label>To: </label>
-			<input class="form-control" type="email" name="sendTo">
-			<br>
-			<label>Subject: </label>
-			<input class="form-control" type="text" name="subjectEmail">
-			<br>
-			<label>Message: </label>
-			<textarea class="form-control" name="contentEmail"></textarea>
-			<br>
-			<input class="btn" type="submit" name="submitFormEmail" value="Send">
-		</form>
-		<div class="result"></div>
-	</div>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-	<script>
-		$(function() { 
-			$("#formEmail").submit(function(event){
-				event.preventDefault();
-				$.ajax({
-					url: "functionSendEmail.php",
-					data: $("#formEmail").serialize(),
-					type: "POST",
-					success: function(data) {
-						$(".result").html(data);
-					}
-				});
-			});
-		});
-	</script>
+    <div class="container">
+        <h2>Send Email</h2>
+        <form method="POST" id="formEmail">
+            <label>To: </label>
+            <input class="form-control" type="email" name="sendTo">
+            <br>
+            <label>Subject: </label>
+            <input class="form-control" type="text" name="subjectEmail">
+            <br>
+            <label>Message: </label>
+            <textarea class="form-control" name="contentEmail"></textarea>
+            <br>
+            <input class="btn" type="submit" name="submitFormEmail" value="Send">
+        </form>
+        <div class="result"></div>
+    </div>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script>
+        $(function() { 
+            $("#formEmail").submit(function(event){
+                event.preventDefault();
+                $.ajax({
+                    url: "functionSendEmail.php",
+                    data: $("#formEmail").serialize(),
+                    type: "POST",
+                    success: function(data) {
+                        $(".result").html(data);
+                    }
+                });
+            });
+        });
+    </script>
 </body>
 </html>
