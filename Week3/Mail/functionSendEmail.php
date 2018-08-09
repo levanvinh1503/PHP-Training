0d798439855cd024f59a02ac43ee2d4de6737c7a
levanvinh1503 <levanvinh1503@gmai.com>
2018-08-09 08:58:14 +0700

    Fix error email 09/08/2018 - 09:00

--- Week3/Mail/functionSendEmail.php
+++ Week3/Mail/functionSendEmail.php
@@ -5,6 +5,7 @@ if ($_POST['sendTo'] && $_POST['subjectEmail'] && $_POST['contentEmail']) {
     $contentEmail = $_POST['contentEmail'];
     $headerEmail = "MIME-Version: 1.0\r\n";
     $headerEmail .= "Content-type: text/html\r\n";
+    
     $resultSendEmail = mb_send_mail($sendTo, $subjectEmail, $contentEmail, $headerEmail);
     if ($resultSendEmail == true) {
         echo "Send email success !";
